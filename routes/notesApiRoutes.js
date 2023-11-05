b7d8d02e35615bd9a5165cd4b0314eb893e9f72f
Taz Whitaker <tazwhitaker@Tazs-MacBook-Air.local>
2023-11-05 17:27:44 -0500

    final update on note js

--- routes/notesApiRoutes.js
+++ routes/notesApiRoutes.js
@@ -1,95 +1,167 @@
-// Importing the required packeges
+// Importing the required packages 
 const express = require('express');
 const notes = require('express').Router();
 const path = require('path');
 const fs = require('fs');
 const uuid = require('../helper/uuid');
-const requst = require('request');
+const request = require('request');
 const axios = require('axios');
 const { compileFunction } = require('vm');
-require ('dotenv').config();
+require('dotenv').config();
 
 //Setting up the express middleware
 notes.use(express.json());
 notes.use(express.urlencoded({ extended: true }));
 
-//GET Route for retrieving all the notes
+// GET Route for retrieving all the notes
 notes.get('/', (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            res.status(500).json({ error: 'Error reading notes' });
-        } else {
-            res.json(JSON.parse(data));
-        }
-    });
-});
-
-
-//POST Route for save the note
-notes.post('/', (req, res) => {
-    const { title, text } = req.body;
-
-    if (title && text) {
-        const newNote = {
-            title,
-            text,
-            id: uuid()
-        };
-
-        fs.readFile('./db/db.json', 'utf8', (err, data) => {
-            if (err) {
-                console.error(err);
-                res.status(500).json({ error: 'Error reading notes' });
-            } else {
-                const notes = JSON.parse(data);
-                notes.push(newNote);
-                fs.writeFile('./db/db.json', JSON.stringify(notes, null, 4), writeErr => {
-                    if (writeErr) {
-                        console.error(writeErr);
-                        res.status(500).json({ error: 'Error saving note' });
-                    } else {
-                        const response = {
-                            status: 'success',
-                            body: newNote
-                        };
-                        console.log(response);
-                        res.status(201).json(response);
-                    }
-                });
-            }
-        });
+  fs.readFile("./db/db.json", 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
     } else {
-        res.status(400).json('Error in posting notes');
+      // Convert string into JSON object
+      res.json(JSON.parse(data));
     }
-});
+  });
+  });
 
-//DELETE Route to delete the note by id
-notes.delete('/:id', (req, res) => {
-    console.log('In deleteroute');
+  //POST route for save the notes 
+  notes.post('/',(req,res) => {
+
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  //If both title and text are present, then process the data and upload to DB
+  if (title && text) {
+    // Variable for the object we will save
+    const newNotes = {
+      title,
+      text,
+      id:uuid()
+    };
+
+    //Get the data from the database file
+    fs.readFile("./db/db.json", 'utf8', (err, data) => {
+      if (err) {
+        console.error('are we erroring out' + err);
+      } else {
+        // Convert string into JSON object
+        const parsedNotes = JSON.parse(data);
+        parsedNotes.push(newNotes);
+      
+      fs.writeFile(
+        './db/db.json',
+        JSON.stringify(parsedNotes, null, 4),
+        (writeErr) =>
+          writeErr
+            ? console.error(writeErr)
+            : console.info('Successfully updated reviews!')
+      )};
+      const response = {
+        status: 'success',
+        body: newNotes,
+      };
+      console.log(response);
+      res.status(201).json(response);
+    });
+  }
+  else {
+    res.status(400).json('Error in posting review');
+  }
+  });
+
+  // DELETE route to delete the notes by id
+  notes.delete("/:id",(req,res) => {
+    console.log('In deleteroute')
     let notesID = req.params.id;
     if (req.body && req.params.id) {
-        fs.readFile('./db/db.json', 'utf8', (err, data) => {
-            if (err) {
-                console.error('are we erroring out' + err);
-            } else {
-                const parsedNoted4Del = JSON.parse(data);
-                let notesFound = parsedNoted4Del.findIndex(note => note.id === notesID);
-                console.log ('In delete' + notesFound);
-                if (notesFound >=0) {
-                    parsedNotes = parsedNoted4Del.splice(notesFound, 1);
-                    fs.writeFile('./db/db.json', JSON.stringify(parsedNoted4Del, null, 4), (writeErr) => 
-                        writeErr
-                            ? console.error(writeErr) 
-                            : console.log('Successfully deleted notes!')
-                    )
-                    res.json(parsedNoted4Del);
-                }
-            }
-        });
+      fs.readFile("./db/db.json", 'utf8', (err, data) => {
+        if (err) {
+          console.error('are we erroring out' + err);
+        } else {
+          // Convert string into JSON object
+          const parsedNotes4Del = JSON.parse(data);
+          let notesFound = parsedNotes4Del.findIndex(e => e.id == notesID);
+          console.log ('In delete' + notesFound);
+          if (notesFound >=0) {
+            parsedNotes4Del.splice(notesFound,1);
+            fs.writeFile(
+              './db/db.json',
+              JSON.stringify(parsedNotes4Del, null, 4),
+              (writeErr) =>
+                writeErr
+                  ? console.error(writeErr)
+                  : console.info('Successfully deleted notes!')
+            )
+            res.json(parsedNotes4Del);
+          }
+        }
+      });
     } else {
-        console.log('Error in req');
-        res.json('invaild request');
-    }
-})
+      console.log('error in req');
+      res.json('invalid request');
+    };
+  });
 
+// notes.post('/createtemplate', async(req,res)=> {
+//   console.log('In createtemplate');
+//   templateMeta = {
+//     "name": "KnickKnack - New Request Notification",
+//     "title": "KnickKnack - New Request Notification",
+//     "html": "./public/email.html"
+//   }
+//   try {
+//       const resp = await axios.post(
+//       process.env.TRUSTIFI_URL + '/api/i/v1/template', 
+//       templateMeta,
+//       {headers: {
+//         'x-trustifi-key': process.env.TRUSTIFI_KEY,
+//         'x-trustifi-secret': process.env.TRUSTIFI_SECRET,
+//         'Content-Type': 'application/json'
+//       }},
+//     );
+//     res.status(200).json(resp.data);
+//   } catch (err)
+//   {
+//     console.log (err);
+//     res.json('invalid request');
+//   }
+
+// })
+
+notes.post('/sendemail', async (req,res)=> {
+  console.log('In sendemail');
+  console.log ('req.body.requestor_notes ', req.body.requestor_notes);
+  console.log('req.body.requestor_email ', req.body.requestor_email);
+  console.log('req.body.provider_email ', req.body.provider_email);
+  console.log('req.body.provider_name ', req.body.provider_name);
+  const emailData = {
+    "template": {
+      "name": "KnickKnack - New Request Notification",
+      "fields": {
+        "second_field": req.body.requestor_notes,
+        "third_field": req.body.requestor_email
+      }
+    },
+    "recipients": [{"email": req.body.provider_email, "name": req.body.provider_name}]
+  };
+
+  try {
+    const resp = await axios.post(
+    process.env.TRUSTIFI_URL + '/api/i/v1/email', 
+    emailData,
+    {headers: {
+      'x-trustifi-key': process.env.TRUSTIFI_KEY,
+      'x-trustifi-secret': process.env.TRUSTIFI_SECRET,
+      'Content-Type': 'application/json'
+    }},
+  );
+  res.status(200).json(resp.data);
+}
+catch(err) {
+  console.log (err);
+  res.json('invalid request');
+}
+});
+
+module.exports = notes; 
