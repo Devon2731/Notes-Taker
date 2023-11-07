57f0deae8ddf235dc3f25625a844c739deae3fef
Taz Whitaker <tazwhitaker@Tazs-MacBook-Air.local>
2023-11-07 13:48:52 -0500

    new updates

--- server.js
+++ server.js
@@ -1,29 +1,18 @@
-const express = require('express');
-const path = require('path');
-const api = require('./routes/index.js');
-
-//Setting up PORT and express application
-const PORT = process.env.PORT || 3001;
+const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3001;
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use('/api', api);
 app.use(express.static('public'));
+// parse incoming string or array data
+app.use(express.urlencoded({extended: true}));
+// parse incoming JSON data
+app.use(express.json());
 
-//HTML get Routes
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
-
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
